f6a8ddffb49b57c59c88803803acbcca41a5213a
WillaQ. <57160489+WillaQ@users.noreply.github.com>
2024-11-11 09:25:34 -0600

    Update app.py

--- app.py
+++ app.py
@@ -60,13 +60,13 @@ if st.session_state.problems and st.session_state.current_problem_index < len(st
     st.write(current_problem)
     #user_answer = st.number_input("Your answer:", step=0.1, key="answer_input")
     # 获取用户输入并尝试立即转换为浮点数
-    user_answer = st.text_input("Your answer:", value="", key="answer_input")  # 使用text_input，默认值为空
+    user_answerori = st.text_input("Your answer:", value="", key="answer_input")  # 使用text_input，默认值为空
     
     # 尝试将输入的字符串转换为浮点数
     try:
-        user_answer_float = float(user_answer.strip().replace(",", ".")) if user_answer else None
+        user_answer = float(user_answerori.strip().replace(",", ".")) if user_answer else None
     except ValueError:
-        user_answer_float = None
+        user_answer = None
         st.warning("Please enter a valid number.")
     
 
